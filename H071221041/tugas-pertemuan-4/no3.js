93cf34f56e6c912f311bdbaf60d89d84ad0afe1f
andimuthia <andimuthiamlptr@gmail.com>
2023-10-06 15:02:22 +0800

    menambah tugas 4

---
+++ H071221041/tugas-pertemuan-4/no3.js
@@ -0,0 +1,11 @@
+// no 3
+let str = prompt('Masukkan kata')
+let strs = str.split('')
+let newstr = strs.reverse()
+newstr = newstr.join('')
+if (str == newstr) {
+    alert('Palindrom')
+}
+else {
+    alert('Bukan Palindrom')
+} 
